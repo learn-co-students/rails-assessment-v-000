48a865b474d34041eba8940224cbd94da6ecd453
Brittany Morris <brittmmendez@gmail.com>
2018-03-07 03:09:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def index
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || root_path
+  end
 
+  def index
   end
 
 
