9e4d47080e91e14e63edf53ec25ccacc03b39fc8
Randall Robinson <keith_adams007@yahoo.com>
2018-04-14 23:01:08 +0000

    removed devise, to much work for so little

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :games
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'welcome#home'
+  root 'welcome#home'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,11 +29,8 @@ ActiveRecord::Schema.define(version: 20180414223958) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.string "user_name"
+    t.string "password_digest"
   end
 
 end
