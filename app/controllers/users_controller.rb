03d57b50b5b66109742506d28f126052749287a6
Randall Robinson <keith_adams007@yahoo.com>
2018-04-15 22:26:38 +0000

    setting up new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
 
+   def new
+     @user = User.new
+   end
 
+   def create
+     @user = User.new(user_params)
+     if @user.valid?
+       @user.save
+       session[:user_id] = @user.id
+       redirect_to user_path(@user)
+     else
+       render :new
+     end
+   end
 
 end
