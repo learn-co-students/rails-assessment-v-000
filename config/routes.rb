2c000ac72def0992d115717d311d4017e5d5176a
Randall Robinson <keith_adams007@yahoo.com>
2018-04-15 21:39:04 +0000

    added sessions & nested route for guides

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :games
+  resources :games do
+    resources :guides
+  end
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
-
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 end
