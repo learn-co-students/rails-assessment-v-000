4607b376fe67a10491563ccd054869699ce669d9
Elle Faraday <faraday.elle@gmail.com>
2018-08-07 19:46:51 +0000

    finish omniauth intial setup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def create
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.image = auth['info']['image']
+    end
+    session[:user_id] = @user.id
+
+    render '/'
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
