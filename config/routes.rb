ccc9c3fb5230ffd49faa4180e22b917708ae8568
Elle Faraday <faraday.elle@gmail.com>
2018-08-07 19:44:00 +0000

    omniauth setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'application#welcome'
+  get '/auth/facebook/callback' => 'sessions#create'
 end
