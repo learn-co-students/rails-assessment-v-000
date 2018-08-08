166461446b48852b990c4db16d4378f18969d2df
Elle Faraday <faraday.elle@gmail.com>
2018-08-08 19:28:02 +0000

    show only available timeslots

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,4 +1,13 @@
 class TimeSlot < ApplicationRecord
   has_many :user_time_slots
-  has_many :user, through: :user_time_slots
+  has_many :users, through: :user_time_slots
+
+  def full
+    self.users.count == self.number_of_volunteers_needed
+  end
+
+  def slots_open
+    self.number_of_volunteers_needed - self.users.count
+  end
+  
 end
