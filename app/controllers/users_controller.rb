5f7a2bbf432345345ffb0ee1b39b5888f50c7e71
Elle Faraday <faraday.elle@gmail.com>
2018-08-08 19:05:25 +0000

    add user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,10 @@ class UsersController < ApplicationController
     @user = User.find(session[:user_id])
   end
 
+  def index
+    @user = User.all
+  end
+
   private
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :name)
