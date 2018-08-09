dee313f1bcea2b0bd5a32fda64eba3dba8096ad4
ellefaraday <faraday.elle@gmail.com>
2018-08-09 15:59:44 -0400

    change timeslot index to be sign up form

--- app/controllers/time_slots_controller.rb
+++ app/controllers/time_slots_controller.rb
@@ -2,6 +2,7 @@ class TimeSlotsController < ApplicationController
 
   def index
     @available_time_slots = TimeSlot.all_available
+    @user_time_slot = UserTimeSlot.new
   end
 
 end
--- app/controllers/user_time_slots_controller.rb
+++ app/controllers/user_time_slots_controller.rb
@@ -3,4 +3,14 @@ class UserTimeSlots < ApplicationController
   def new
   end
 
+  def create
+    @user_time_slot = UserTimeSlot.create(user_time_slot_params)
+
+    redirect_to user_path(@user)
+  end
+
+  def user_time_slot_params
+    params.require(:user_time_slot).permit(:time_slot_id)
+  end
+
 end
