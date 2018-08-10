4bfaebd5bec3b5401a7347f53a9f50de29f79dfe
ellefaraday <faraday.elle@gmail.com>
2018-08-10 16:20:14 -0400

    require login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+  skip_before_action :require_login, only: [:welcome]
+
   def welcome
     if session[:user_id]
       @user = User.find(session[:user_id])
     end
+  end
+
+  private
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
   end
 end
--- app/controllers/time_slots_controller.rb
+++ app/controllers/time_slots_controller.rb
@@ -1,5 +1,4 @@
 class TimeSlotsController < ApplicationController
-
   def index
 
   end
