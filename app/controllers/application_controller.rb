27a01d360ee08e40fad89954a5b1bf4ed13aa876
ellefaraday <faraday.elle@gmail.com>
2018-08-10 16:31:14 -0400

    if logged in don't go to / page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   def welcome
     if session[:user_id]
       @user = User.find(session[:user_id])
+      redirect_to user_path(@user)
     end
   end
 
