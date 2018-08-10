e303d05cb5034f2650439041bc9481cba7a19351
ellefaraday <faraday.elle@gmail.com>
2018-08-10 13:51:58 -0400

    broke up time and date

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -20,8 +20,12 @@ class TimeSlot < ApplicationRecord
     available
   end
 
+  def readable_date
+    self.start_time.strftime("%B %d, %Y")
+  end
+
   def readable_start_time
-    self.start_time.strftime("%B %d, %Y at %I:%M %p")
+    self.start_time.strftime("%I:%M %p")
   end
 
   def duration_in_hours
@@ -33,7 +37,7 @@ class TimeSlot < ApplicationRecord
   end
 
   def readable_end_time
-    self.end_time.strftime("%B %d, %Y at %I:%M %p")
+    self.end_time.strftime("%I:%M %p")
   end
 
 end
