f9f016ca74d20acdf77f68e76f92f94a3c45282c
Michael Pope <popemray1986@gmail.com>
2018-09-25 01:18:27 +0000

    Done.

--- workout/app/controllers/posts_controller.rb
+++ workout/app/controllers/posts_controller.rb
@@ -22,7 +22,16 @@ class PostsController < ApplicationController
   end
 
   def show
-    @posts = Post.find(params[:id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path
+    else
+      render :edit
+    end
   end
 
   private
--- workout/app/models/user.rb
+++ workout/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :goals
   has_many :posts, through: :goals
+
+  add validations
+
   has_secure_password
 end
