7e8ef8d832a9e329aef98b03f6a2ee2fccf59742
ellefaraday <faraday.elle@gmail.com>
2019-01-29 17:49:20 -0500

    fix user_time_slot edit

--- app/controllers/user_time_slots_controller.rb
+++ app/controllers/user_time_slots_controller.rb
@@ -22,18 +22,8 @@ class UserTimeSlotsController < ApplicationController
 
   def edit
     set_user
-    @user_time_slot = UserTimeSlot.find_by(user_id: params[:user_id], time_slot_id: params[:id])
+    @user_time_slot = UserTimeSlot.find_by_id(params[:id])
     @time_slot = TimeSlot.find(params[:id])
-  end
-
-  def destroy
-    if permission_check
-      UserTimeSlot.find(params[:id]).destroy
-      set_user
-      redirect_to "/users/#{@user.id}/time_slots"
-    else
-      redirect_to '/'
-    end
   end
 
   def update
@@ -41,6 +31,16 @@ class UserTimeSlotsController < ApplicationController
       @user_time_slot = UserTimeSlot.find(params[:id])
       @user_time_slot.comments = params[:user_time_slot][:comments]
       @user_time_slot.save
+      set_user
+      redirect_to "/users/#{@user.id}/time_slots"
+    else
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    if permission_check
+      UserTimeSlot.find(params[:id]).destroy
       set_user
       redirect_to "/users/#{@user.id}/time_slots"
     else
